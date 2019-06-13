74e7e0a4d8ad50ce082a9f07fe9d22df28d2671d
Marisa Alves <marisa.alves@mail.concordia.ca>
2019-06-13 10:51:14 -0400

    Added scatterplot for bp,bmi,tch

--- Python_Scripts/Exploratory_Analysis.py
+++ Python_Scripts/Exploratory_Analysis.py
@@ -18,7 +18,7 @@ diabetes_df.columns = ['Age', 'Sex', 'BMI', 'BP', 'map', 'tc', 'ldl', 'hdl', 'tc
 diabetes_unaltered = pd.read_csv('diabetes.data',
                           sep='\s+',
                           header=0)
-print(diabetes_unaltered.describe().to_string())
+diabetes_unaltered.describe().to_csv("Diabetes_Describe")
 
 os.makedirs('plots', exist_ok=True)
 
@@ -39,6 +39,7 @@ plt.savefig('plots/diabetes_heatmap.png')
 
 plt.close()
 
+#Boxplots
 fig, axes = plt.subplots(ncols=5, nrows=2, figsize=(10, 5))
 for indx, col in enumerate(diabetes_unaltered.columns):
     outliers = dict(markerfacecolor='r', marker='D')
@@ -58,3 +59,17 @@ fig.tight_layout()
 fig.subplots_adjust(top=0.88)
 plt.savefig(f'plots/Boxplots_features')
 plt.clf()
+
+#Scatter Plot
+fig, axes = plt.subplots(1, 1, figsize=(5, 5))
+axes.grid(axis='y', alpha=0.5)
+axes.scatter(diabetes_df["tch"], diabetes_df["Target"], marker="1", color='blue')
+axes.scatter(diabetes_df["BP"], diabetes_df["Target"], marker="*", color='orange')
+axes.scatter(diabetes_df["BMI"], diabetes_df["Target"], marker=".", color='green')
+axes.set_title(f'Diabetes comparisons')
+axes.set_ylabel('Diabetes Progression Indicator')
+axes.set_xlabel('Feature Levels')
+axes.legend(("tch", "BP", "BMI"))
+plt.savefig(f'plots/diabetesProgression_to_tch_BP_BMI.png', dpi=300)
+plt.clf()
+plt.close()
