49b2e5cfb2caf3c841797d5258b9b26495da71e2
Marisa Alves <marisa.alves@mail.concordia.ca>
2019-06-13 22:38:51 -0400

    Combining exploratory analysis with lasso in one file

--- Python_Scripts/Lasso_Regression.py
+++ Python_Scripts/Lasso_Regression.py
@@ -11,20 +11,81 @@ from scipy import stats
 
 os.makedirs('plots/', exist_ok=True)
 
+#Loading the dataset from sklearn
 diabetes = load_diabetes()
 
 diabetes_df = pd.DataFrame(data=np.c_[diabetes.data, diabetes.target], columns=diabetes.feature_names + ['target'])
 diabetes_df.columns = ['Age', 'Sex', 'BMI', 'BP', 'map', 'tc', 'ldl', 'hdl', 'tch', 'glu', 'Target']
 
+
+#Exploratory analysis on dataset
+diabetes_unaltered = pd.read_csv('diabetes.data',
+                          sep='\s+',
+                          header=0)
+diabetes_unaltered.describe().to_csv("Diabetes_Describe.csv")
+
+os.makedirs('plots', exist_ok=True)
+
+sns.set(style='darkgrid', palette='coolwarm')
+
+#Pairplot for differences between sexes
+sns.pairplot(diabetes_df, hue='Sex', diag_kind='hist')
+plt.savefig('plots/diabetes_pairplot.png')
+plt.clf()
+
+#Heatmap
+fig, ax = plt.subplots(figsize=(12,12))
+sns.heatmap(diabetes_df.corr(), annot=True, cmap='RdYlGn')
+ax.set_xticklabels(diabetes_df.columns, rotation=45)
+ax.set_yticklabels(diabetes_df.columns, rotation=45)
+plt.title('Heatmap for Diabetes Variables')
+plt.savefig('plots/diabetes_heatmap.png')
+
+#Boxplots for distributions of features
+fig, axes = plt.subplots(ncols=5, nrows=2, figsize=(10, 5))
+for indx, col in enumerate(diabetes_unaltered.columns):
+    outliers = dict(markerfacecolor='r', marker='D')
+    if col != 'Sex':
+        if indx == 0:
+            axes[0][indx].boxplot(diabetes_unaltered[col], flierprops=outliers)
+            axes[0][indx].set_ylabel(col)
+        elif indx <= 5 and indx >=2:
+            axes[0][6-indx].boxplot(diabetes_unaltered[col], flierprops=outliers)
+            axes[0][6-indx].set_ylabel(col)
+        elif indx <=10 and indx>=6:
+            axes[1][10-indx].boxplot(diabetes_unaltered[col], flierprops=outliers)
+            axes[1][10-indx].set_ylabel(col)
+fig.suptitle("Boxplots for Diabetes Features")
+fig.tight_layout()
+fig.subplots_adjust(top=0.88)
+plt.savefig(f'plots/Boxplots_features')
+plt.clf()
+
+#Scatter Plot
+fig, axes = plt.subplots(1, 1, figsize=(5, 5))
+axes.grid(axis='y', alpha=0.5)
+axes.scatter(diabetes_df["tch"], diabetes_df["Target"], marker="1", color='blue')
+axes.scatter(diabetes_df["BP"], diabetes_df["Target"], marker="*", color='orange')
+axes.scatter(diabetes_df["BMI"], diabetes_df["Target"], marker=".", color='green')
+axes.set_title(f'Diabetes comparisons')
+axes.set_ylabel('Diabetes Progression Indicator')
+axes.set_xlabel('Feature Levels')
+axes.legend(("tch", "BP", "BMI"))
+plt.savefig(f'plots/diabetesProgression_to_tch_BP_BMI.png', dpi=300)
+plt.clf()
+plt.close()
+
+#Lasso Regression analysis
+
+#Dummy variable for sex feature
 encoded_sex = pd.get_dummies(diabetes_df['Sex'], drop_first=True)
 diabetes_df = pd.concat([diabetes_df, encoded_sex], axis=1)
 diabetes_df.rename(columns = {list(diabetes_df)[11]: "Encoded Sex"}, inplace=True)
 diabetes_df.drop(['Sex'], axis=1, inplace=True)
 
+#Removing outliers based on Z Score
 z = np.abs(stats.zscore(diabetes_df))
-
 diabetes_df_o = diabetes_df[(z < 3).all(axis=1)]
-
 X = diabetes_df_o.loc[:, ['Age', 'BMI', 'BP', 'map', 'tc', 'ldl', 'hdl', 'tch', 'glu', 'Encoded Sex']]
 y = diabetes_df_o['Target']
 
@@ -67,6 +128,7 @@ plt.plot([0, 0], [50, 0], '--')
 plt.title('Lasso Residual Distribution')
 plt.savefig('plots/Lasso_Residual_Distn.png')
 plt.clf()
+plt.close()
 
 print(f"MAE error(avg abs residual): {metrics.mean_absolute_error(y_test, predicted_values)}")
 print(f"MSE error: {metrics.mean_squared_error(y_test, predicted_values)}")
